3dd1f977d3660980c47e7209936cc00cbd28940b
Steven Ackley <stevenfackley@gmail.com>
2018-06-23 19:55:56 -0400

    Gallery updates

--- BJJInParadise.Web/Controllers/GalleryController.cs
+++ BJJInParadise.Web/Controllers/GalleryController.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Imgur.API;
+using Imgur.API.Authentication.Impl;
+using Imgur.API.Endpoints.Impl;
+using Imgur.API.Models;
 
 namespace BJJInParadise.Web.Controllers
 {
@@ -12,20 +19,37 @@ namespace BJJInParadise.Web.Controllers
         // GET: Gallery
         public ActionResult Index()
         {
-            var dir = Server.MapPath("/Images/Images");
-            var retVal = new List<string>();
           
-            string projectFolder = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).FullName;
-            string folderAppData = Path.Combine(projectFolder, "Images");
-            if (Directory.Exists(folderAppData))
+            return View();
+        }
+
+        private async Task<IEnumerable<ImageResult>> GetImagesAsync()
+        {
+            try
             {
-                foreach (var file in Directory.EnumerateFiles(folderAppData))
-                {
-                    retVal.Add("~/Images/" + Path.GetFileName(file));
-                }
+                var client = new ImgurClient("bc01b8dbb915f5f", "07927d3b0dfe5c530d663d89b9110100f7a1d506");
+                var endpoint = new AlbumEndpoint(client);
+                var image = await endpoint.GetAlbumImagesAsync("SZFpO6D");
+             
+                return image.Select(x => new ImageResult {Link = x.Link,Description = x.Description}).ToList();
+            }
+            catch (ImgurException imgurEx)
+            {
+                Debug.Write("An error occurred getting an image from Imgur.");
+                Debug.Write(imgurEx.Message);
             }
 
-            return View(retVal);
+            return null;
         }
+
+     
+
+     
+    }
+
+    public class ImageResult
+    {
+        public string Link { get; set; }
+        public string Description { get; set; }
     }
 }
